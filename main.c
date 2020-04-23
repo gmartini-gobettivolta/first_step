fa1138f06a71e274987d7deb019c576852c0551b
Giancarlo <g.martini@gobettivolta.gov.it>
2020-04-23 08:38:54 +0100

    Inserimento del file main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+/* 	Progetto: Ripasso programmazione in c
+ *  	Nome file: main.c
+ *  	Autore: Giancarlo Martini
+ *  	Descrizione: Ripasso keyword c
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	puts("Ciao\n");
+	int pippo = 37;
+	printf("Ciao %d\n",pippo);
+	return 0;
+}
+
+
